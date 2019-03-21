f43745f4037709e088f77cf093081947def5bae1
Jeet <jeet86.soni@gmail.com>
2019-03-21 13:51:45 +0545

    added one search customer file and modify edit_customer

--- src/test/java/com/freecrm/qa/testcases/TC_Edit_Customer_001.java
+++ src/test/java/com/freecrm/qa/testcases/TC_Edit_Customer_001.java
@@ -1,2 +1,4 @@
 //This is a new Test case 
 //this test will edit and update check in Git
+
+//this is code added newly
---
+++ src/test/java/com/freecrm/qa/testcases/Tc_Search_Customer_002.java
@@ -0,0 +1,6 @@
+package com.freecrm.qa.testcases;
+
+public class Tc_Search_Customer_002 {
+//This is my Search customer Test case
+	
+}
